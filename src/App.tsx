f51e20512f83d5b99815ca8ebb94161aa8b00c2e
fumiyamiyazaki <57476802+fumiyamiyazaki@users.noreply.github.com>
2022-03-13 23:39:14 +0900

    definition type FC,VFC,use ?

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,41 @@ import axios from "axios";
 import { useState } from "react";
 import "./styles.css";
 
+import { Todo } from "./Todo";
+import { Text } from "./Text";
+import { UserProfile } from "./UserProfile";
+import { TodoType } from "./types/todo";
+import { User } from "./types/user";
+
+const user: User = {
+  name: "テスト"
+  // hobbies: ["映画", "ゲーム"],
+};
+
 export default function App() {
-  const [todos, setTodos] = useState<any>([]);
+  const [todos, setTodos] = useState<Array<TodoType>>([]);
+
   const onClickFetchData = () => {
-    axios.get("https://jsonplaceholder.typicode.com/todos").then((res) => {
-      console.log(res);
-    });
+    axios
+      .get<Array<TodoType>>("https://jsonplaceholder.typicode.com/todos")
+      .then((res) => {
+        setTodos(res.data);
+      });
   };
 
   return (
     <div className="App">
+      <UserProfile user={user} />
+      <Text color="red" fontSize="20px" />
       <button onClick={onClickFetchData}>データ取得</button>
+      {todos.map((todo) => (
+        <Todo
+          key={todo.id}
+          title={todo.title}
+          userId={todo.userId}
+          completed={todo.completed}
+        />
+      ))}
     </div>
   );
 }
